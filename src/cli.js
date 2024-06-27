819d018739387360252abd537f32c4c603c2bd02
sovuix <sovuix@gmail.com>
2024-06-27 14:22:22 +0300

    Add func GetUserName

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from "readline-sync";
+
+export const getUserName = () => {
+  const userName = readlineSync.question("May I have your name?");
+  return console.log(`Hello, ${userName}!`);
+};
