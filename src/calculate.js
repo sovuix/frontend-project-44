38188d90fb7bd00c3758707cd9457e9b3660fe94
sovuix <sovuix@gmail.com>
2024-07-03 10:23:02 +0300

    fix calculate.js

--- src/calculate.js
+++ src/calculate.js
@@ -1,11 +1,10 @@
-import readlineSync from 'readline-sync';
 import { getUserName } from '../src/greeting.js';
 import { getRandomNum } from './getRandomNum.js';
 import { getTheQuestion } from './getTheQuestion.js';
 
 const getShuffle = (arr) => {
   let j, temp;
-  for (var i = arr.length - 1; i > 0; i--) {
+  for (let i = arr.length - 1; i > 0; i--) {
     j = Math.floor(Math.random() * (i + 1));
     temp = arr[j];
     arr[j] = arr[i];
@@ -26,30 +25,46 @@ export const calc = () => {
   while ((tryCount < 3) && (result = true)) {
     const num1 = getRandomNum();
     const num2 = getRandomNum();
-  
-    // const shuffle = getShuffle(operator);
-    const expression = `${num1}${shuffle[tryCount]}${num2}`;
+    const expression = `${num1} ${shuffle[tryCount]} ${num2}`;
+    const userAnswer = parseInt(getTheQuestion(expression));
 
-
-    const userAnswer = getTheQuestion(expression);
-    console.log(expression);
-    
-    switch (true) {
-      case (parseInt(userAnswer) === parseInt(expression)):
-        console.log('Correct!');
-        tryCount += 1;
-        break;
-      case (parseInt(userAnswer) !== parseInt(expression)):
-        console.log(`${parseInt(userAnswer)} is wrong answer ;(. Correct answer was ${parseInt(expression)}.`);
-        console.log(`Let's try again, ${userName}`)
-        return result = true;
+    switch (shuffle[tryCount]) {
+      case '+':
+        if (userAnswer === (num1 + num2)) {
+          console.log('Correct!');
+          tryCount += 1;
+          break;
+        } else {
+          console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${num1 + num2}.`);
+          console.log(`Let's try again, ${userName}`);
+          return result = true;
+        }
+      case '-':
+        if (userAnswer === (num1 - num2)) {
+          console.log('Correct!');
+          tryCount += 1;
+          break;
+        } else {
+          console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${num1 - num2}.`);
+          console.log(`Let's try again, ${userName}`);
+          return result = true;
+        }
+      case '*':
+        if (userAnswer === (num1 * num2)) {
+          console.log('Correct!');
+          tryCount += 1;
+          break;
+        } else {
+          console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${num1 * num2}.`);
+          console.log(`Let's try again, ${userName}`);
+          return result = true;
+        }
       default:
-        console.log(`Let's try again, ${userName}`)
+        console.log(`Let's try again, ${userName}`);
         return result = true;
     }
   }
   console.log(`Congratulations, ${userName}!`);
 }
-  
 
 
